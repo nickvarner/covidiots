c5eb2df9aebad0d7fd3b100675abb7297bc7e78d
[nick varner] <nick.varner@gmail.com>
2021-06-24 10:19:20 -0700

    started profile reducer and route, but switching gears for a sec

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,8 +4,12 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
+import submission from './submission';
 
 export default combineReducers({
 	alert,
-	auth
+	auth,
+	profile,
+	submission
 });
---
+++ client/src/reducers/profile.js
@@ -0,0 +1,5 @@
+const initialState = {
+  profile: null,
+  profiles: [];
+  
+}
